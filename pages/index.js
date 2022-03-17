b0aa38b121cedf9c78be89489120c91de6a8f27f
SeyoungCho <wnl383@naver.com>
2022-03-17 22:25:17 +0900

    added custom app and added global styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import NavBar from "../components/Navbar"
 export default function Home(){
   return(
     <div>
-      <NavBar />
       <h1 className="active">Hello</h1>
+      <style jsx global>
+        {`
+          a {
+            color:white;
+          }
+        `}
+      </style>
     </div>
   )
 }
